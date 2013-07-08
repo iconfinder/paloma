56e561798c97ff74bcdbcc570ce3435af4cbb4e7
Nick Bruun <nick@bruun.co>
2013-07-08 13:10:31 +0200

    Bump to version 1.0.1 for coffin support.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ tests_require = [
 
 setup(
     name='django-paloma',
-    version='1.0.0',
+    version='1.0.1',
     description='Class based e-mails for Django',
     author='Nick Bruun',
     author_email='nick@bruun.co',
